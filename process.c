02ecbeed39e9bd7aa1b4905337044781e4219059
itasahobby <itasahobby@gmail.com>
2020-03-20 21:05:49 +0100

    Added sleep and wakeup process, together with a basic context switch function.

---
+++ process.c
@@ -0,0 +1,53 @@
+/*
+Changes the state of the process from:
+    ASLEEP_IN_MEM   ->  READY_TO_RUN
+    KERNEL_RUNNING  ->  ASLEEP_IN_MEM
+
+Usually gets called due to a syscall,a context switch is needed afterwards.
+Another reason can be a pagination error, and it's not in memory.
+
+If an event occurs they leave the ASLEEP_IN_MEM state, they change to AWAKEN into READY_TO_RUN.
+Then we put it back in virtual memory. This events are supposed to be in the kernel
+and have asigned a specific memory direction.
+
+When a process waits for the buffer has to wait for the I/O to finish. While it's not done
+they will be in the ASLEEP_IN_MEM state.
+
+*/
+
+int sleep (  Proc *proc , int priority ){
+
+    pushcli();                                      // Blocks all interruptions
+    process_toSleep(proc);                          // Put a process to sleep 
+    insert_SleepList(proc,list);                    // Adds the process to the slept processes list
+
+    if (process is not interrumptable){                     
+        switch_context();                           // Process resumes the execution when it wakes up   
+        popcli();                                   // Unblocks all interruptions
+        return 0;
+    }
+
+    if (proc->signals == NULL){                     // No signal pending against the process
+        switch_context();                           // Process resumes the execution when it wakes up 
+    }
+
+    remove_SleepList(proc,list);                    // Removes the process from the list
+
+    if(proc->state == AWAKEN) return 1;
+    
+    longjmp_algorithm();                        
+}
+
+void wake_up (Proc *proc){
+    
+    pushcli();                                      // Blocks all interruptions
+    remove_SleepList(proc,list);                    // Removes the process from the list 
+    proc->state = READY_TO_RUN;                     // Set the process state for READY_TO_RUN
+    insert_SchedulerList(process);                  // Linked List Elligible process
+    if(proc->state == ASLEEP){ 
+        swap_in(proc);                              // Kernel swap the process into RAM
+    } else{
+        scheduler();                                // Decide which process to run after waking this one up
+    } 
+    popcli();
+}
--- process.h
+++ process.h
@@ -8,12 +8,31 @@ typedef struct{
 } Process_timer;
 
 typedef struct{
-    int state;              // Process state
-    int dir;                // Direction in memory of the process
-    int uid;                // User id
-    int pid;                // Process id
-    int awake;              // 0 awake 1 sleep
-    int nice;               // Scheduling parameters
-    Signals *signals;       // List of signals received but not processed
-    Process_timer timers;   // Accounting
-} Process_info;
+    int state;                  // Process state
+    int dir;                    // Direction in memory of the process
+    char *kstack;               // Bottom of kernel stack for this process
+    int uid;                    // User id
+    int pid;                    // Process id
+    Proc * parent;              // Parent process
+    Context * u_context;        // Context of the process in user mode
+    Context * k_context;        // Context of the process in kernel mode
+    int awake;                  // 0 awake 1 sleep
+    int nice;                   // Scheduling parameters
+    Signals *signals;           // List of signals received but not processed
+    Process_timer timers;       // Accounting
+} Proc;
+
+typedef Proc  *Process_table;   // Table that contains all processes
+
+/* Set the process to sleep mode, also adds it to a list that contains all processes in that state.
+    PARAMETERS:
+        Proc * proc     : Pointer to the process you want to sleep
+        int priority    : Priority of the process 
+        
+        * The priority it may get removed since we have a similar field in Proc (nice field) which can do the same job.
+
+    RETURNS:
+        int             : Returns 0 if the process has been slept sucessfully, otherwise sends 1.
+*/
+int sleep (  Proc * proc , int priority );
+void wake_up (Proc *proc);
